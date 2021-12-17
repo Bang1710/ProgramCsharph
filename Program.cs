47cca50e48dc84e88770bfd31b3395439e7f915b
Bang1710 <87267619+Bang1710@users.noreply.github.com>
2021-12-17 08:35:49 +0700

    file test c#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace File_code_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int S = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (i % 2== 0)
+                {
+                    S+=i;
+                }
+            }
+            Console.WriteLine("Tong cua nhung so chan {0} ",S);
+        }
+    }
+}
