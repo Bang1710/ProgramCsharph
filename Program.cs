372bf1d9ae284457057ae57a1465c8a5bb826ad1
Bang1710 <87267619+Bang1710@users.noreply.github.com>
2021-12-17 09:02:09 +0700

    upload code

--- Program.cs
+++ Program.cs
@@ -4,17 +4,28 @@ namespace File_code_C_
 {
     class Program
     {
+        /// <summary>
+        /// Tính tổng hai số nguyên
+        /// </summary>
+        /// <param name="a">số thứ nhất</param>
+        /// <param name="b">số thứ hai</param>
+        /// <returns>giá trị a + b</returns>
+        static int TongHaiSo(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
             int S = 0;
             for (int i = 0; i < 10; i++)
             {
-                if (i % 2== 0)
+                if (i % 2 == 0)
                 {
-                    S+=i;
+                    S += i;
                 }
             }
-            Console.WriteLine("Tong cua nhung so chan {0} ",S);
+            Console.WriteLine("Tong cua nhung so chan {0} ", S);
+            TongHaiSo(10, 12);
         }
     }
 }
